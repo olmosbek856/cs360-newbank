e1e26b7deaef98d480cef683e5dd12175d43ed5c
Azizbek1308 <a.ergashov@newuu.uz>
2026-06-29 17:28:25 +0500

    fixed not found cards with TTD method

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class CardDetailTests(TestCase):
+
+    def test_valid_card_types_return_200(self):
+        """Ruxsat berilgan karta turlari 200 OK qaytarishi kerak"""
+        valid_cards = ['Visa', 'MasterCard', 'Humo', 'Uzcard']
+        for card in valid_cards:
+            # urls.py dagi name='card_detail' ga argument berib chaqiramiz
+            url = reverse('home:card_detail', kwargs={'card_type': card})
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_invalid_card_type_returns_404(self):
+        """Tizimda yo'q karta turi (masalan, Azizbek) kiritilsa 404 qaytarishi kerak"""
+        url = reverse('home:card_detail', kwargs={'card_type': 'Azizbek'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,7 +10,14 @@ def cards(request):
     return render(request, 'home/index.html')
 
 def card_detail(request, card_type):
+    # Ruxsat etilgan kartalar ro'yxati
+    VALID_CARD_TYPES = ['Visa', 'MasterCard', 'Humo', 'Uzcard']
     
+    # Kiritilgan qiymatni ro'yxatdagilar bilan solishtirish (registrni hisobga olmaslik ham mumkin, masalan .title() bilan)
+    # Biz aniq moslikni tekshiramiz:
+    if card_type not in VALID_CARD_TYPES:
+        raise Http404("Bunday turdagi karta mavjud emas!")
+
     context = {
         'card_name': card_type,
     }
